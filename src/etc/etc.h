761bad6d9a202becfa681291fdb4f5ca6bc718a5
Carlo Corradini <carlo.ita98@gmail.com>
2021-12-28 19:14:38 +0100

    Better comments for enum(s)

--- src/etc/etc.h
+++ src/etc/etc.h
@@ -9,8 +9,18 @@
  * @brief Command types.
  */
 typedef enum {
+  /**
+   * @brief Don't do anything (ignore).
+   */
   COMMAND_TYPE_NONE,
+  /**
+   * @brief Sensed value should go to 0, and the threshold back to normal.
+   */
   COMMAND_TYPE_RESET,
+  /**
+   * @brief Sensed value should not be modified, but the threshold should be
+   * increased.
+   */
   COMMAND_TYPE_THRESHOLD
 } command_type_t;
 
@@ -18,8 +28,17 @@ typedef enum {
  * @brief Node roles.
  */
 typedef enum {
+  /**
+   * @brief Controller node.
+   */
   NODE_ROLE_CONTROLLER,
+  /**
+   * @brief Sensor/Actuator node.
+   */
   NODE_ROLE_SENSOR_ACTUATOR,
+  /**
+   * @brief Forwarder node.
+   */
   NODE_ROLE_FORWARDER
 } node_role_t;
 
