e4c7bbc1b039dde8216a968976725abcdf6f39cc
Carlo Corradini <carlo.ita98@gmail.com>
2022-01-06 17:05:54 +0100

    Better testbed

--- scenarios/testbed/get-test.sh
+++ scenarios/testbed/get-test.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash
 TESTID=$1
 
-python $TESTBED_CLIENT download $1
+python3 $TESTBED_CLIENT download $1
 tar -xf 'job_'$TESTID'.tar.gz'
 rm 'job_'$TESTID'.tar.gz'
--- scenarios/testbed/run-test.sh
+++ scenarios/testbed/run-test.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/bash
-python $TESTBED_CLIENT schedule experiment.json 2>&1 | tee last-test.txt
+python3 $TESTBED_CLIENT schedule experiment.json 2>&1 | tee last-test.txt
